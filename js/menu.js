4d466da85f966d139500215ca559bd795a038f55
Lea Nørager <leno0001@stud.kea.dk>
2025-01-07 17:39:42 +0100

    noter til css mobil og små rettelser

--- js/menu.js
+++ js/menu.js
@@ -5,10 +5,12 @@ const menu = document.querySelector(".menu");
 burger.addEventListener("click", burgerClick);
 function burgerClick() {
   burger.classList.toggle("active");
-  nav.classList.toggle("active");
+  nav.classList.toggle("active"); // toggle = tilføjer eller fjerner active-class fra elementet, afhængigt af om den allerede findes på elementet.
 }
-menu.addEventListener("click", menuClick);
+
+menu.addEventListener("click", menuClick); // tilføjer en event listener til elementet menu, så når det bliver klikket på, kaldes funktionen menuClick.
+
 function menuClick() {
   burger.classList.remove("active");
-  nav.classList.remove("active");
+  nav.classList.remove("active"); // active-class fjernes
 }
